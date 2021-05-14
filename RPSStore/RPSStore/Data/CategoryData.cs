d389cfff5e0aafdbc7e46f4d4df5baac097997a7
eswaribala <parameswaribala@gmail.com>
2021-05-14 22:50:24 +0530

    v3

---
+++ RPSStore/RPSStore/Data/CategoryData.cs
@@ -0,0 +1,25 @@
+﻿using RPSStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPSStore.Data
+{
+    public static class CategoryData
+    {
+        public static IList<Category> Categories;
+
+        static CategoryData()
+        {
+            Categories = new List<Category>();
+            Categories.Add(new Category { CategoryId = "XXdyQgAACAAJ", Name = "The C Book, Featuring the ANSI C Standard", Details = "This book presents an introduction to the C programming language, featuring a structured approach and aimed at professionals and students with some experience of high-level languages. Features *includes embedded summary material in bulleted form *highlights common traps and pitfalls in C programming.", ImageUrl = "http://books.google.com/books/content?id=XXdyQgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api" });
+            Categories.Add(new Category { CategoryId = "XXdyQgAACAAJ", Name = "The C Book, Featuring the ANSI C Standard", Details = "This book presents an introduction to the C programming language, featuring a structured approach and aimed at professionals and students with some experience of high-level languages. Features *includes embedded summary material in bulleted form *highlights common traps and pitfalls in C programming.", ImageUrl = "http://books.google.com/books/content?id=XXdyQgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api" });
+            Categories.Add(new Category { CategoryId = "XXdyQgAACAAJ", Name = "The C Book, Featuring the ANSI C Standard", Details = "This book presents an introduction to the C programming language, featuring a structured approach and aimed at professionals and students with some experience of high-level languages. Features *includes embedded summary material in bulleted form *highlights common traps and pitfalls in C programming.", ImageUrl = "http://books.google.com/books/content?id=XXdyQgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api" });
+            Categories.Add(new Category { CategoryId = "XXdyQgAACAAJ", Name = "The C Book, Featuring the ANSI C Standard", Details = "This book presents an introduction to the C programming language, featuring a structured approach and aimed at professionals and students with some experience of high-level languages. Features *includes embedded summary material in bulleted form *highlights common traps and pitfalls in C programming.", ImageUrl = "http://books.google.com/books/content?id=XXdyQgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api" });
+            Categories.Add(new Category { CategoryId = "XXdyQgAACAAJ", Name = "The C Book, Featuring the ANSI C Standard", Details = "This book presents an introduction to the C programming language, featuring a structured approach and aimed at professionals and students with some experience of high-level languages. Features *includes embedded summary material in bulleted form *highlights common traps and pitfalls in C programming.", ImageUrl = "http://books.google.com/books/content?id=XXdyQgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api" });
+
+
+
+        }
+    }
+}
---
+++ RPSStore/RPSStore/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPSStore.Models
+{
+   public class Category
+    {
+        public String CategoryId { get; set; }
+        public String Name { get; set; }
+        public String Details { get; set; }
+        public String ImageUrl { get; set; }
+    }
+}
