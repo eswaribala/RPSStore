2a8f595b7162699bb96ac41fcf7409743cacc7d7
eswaribala <parameswaribala@gmail.com>
2021-05-18 21:47:55 +0530

    v26

--- RPSStore/RPSStore/Views/LatestArrival.xaml.cs
+++ RPSStore/RPSStore/Views/LatestArrival.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using RPSStore.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.CommunityToolkit.UI.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,23 @@ namespace RPSStore.Views
         public LatestArrival()
         {
             InitializeComponent();
+            var vm = new ProductViewModel();
+            this.BindingContext = vm;
+        }
+        void OnLabelTapped(object sender, EventArgs e)
+        {
+            Label label = sender as Label;
+            Expander expander = label.Parent.Parent.Parent as Expander;
+
+            if (label.FontSize == Device.GetNamedSize(NamedSize.Default, label))
+            {
+                label.FontSize = Device.GetNamedSize(NamedSize.Large, label);
+            }
+            else
+            {
+                label.FontSize = Device.GetNamedSize(NamedSize.Default, label);
+            }
+            expander.ForceUpdateSize();
         }
     }
 }
