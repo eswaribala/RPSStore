4b748726e501487e2a4b4cd5fefc3a46722a90e9
eswaribala <parameswaribala@gmail.com>
2021-05-19 23:56:33 +0530

    Form Ready

---
+++ RPSStore/RPSStore/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPSStore.Models
+{
+    public class Contact
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Gender { get; set; }
+        public List<Boolean> ContactModes { get; set; }
+    }
+}
--- RPSStore/RPSStore/Views/RPSBusinessPage.xaml.cs
+++ RPSStore/RPSStore/Views/RPSBusinessPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RPSStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,11 @@ namespace RPSStore.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RPSBusinessPage : ContentPage
     {
+        private String Gender { get; set; }
+        private bool PhoneNo { get; set; }
+        private bool Email { get; set; }
+        private bool WhatsApp { get; set; }
+        private bool FaceBook { get; set; }
         public RPSBusinessPage()
         {
             InitializeComponent();
@@ -19,7 +25,43 @@ namespace RPSStore.Views
 
         private void OnGenderRadioButtonCheckedChanged(object sender, CheckedChangedEventArgs e)
         {
+            RadioButton radioButton = sender as RadioButton;
+            Gender = radioButton.Content.ToString();
+        }
 
+        private void Contact_Clicked(object sender, EventArgs e)
+        {
+            Contact contact = new Contact();
+            contact.FirstName = FirstName.Text;
+            contact.LastName = LastName.Text;
+            contact.Gender = Gender;
+            List<Boolean> modes = new List<Boolean>();
+            modes.Add(PhoneNo);
+            modes.Add(Email);
+            modes.Add(WhatsApp);
+            modes.Add(FaceBook);
+            DisplayAlert("Received", contact.Gender, "OK");
+
+        }
+
+        private void phoneNoCheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            PhoneNo = true;
+        }
+
+        private void emailCheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            Email = true;
+        }
+
+        private void WhatsAppCheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            WhatsApp = true;
+        }
+
+        private void facebookCheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            FaceBook = true;
         }
     }
 }
