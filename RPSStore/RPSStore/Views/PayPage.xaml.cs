32a6a5353dde73cb42b1d2a5a1a3540cf5ad58c7
eswaribala <parameswaribala@gmail.com>
2021-05-19 22:07:57 +0530

    Phone Dialer

--- RPSStore/RPSStore/Views/PayPage.xaml.cs
+++ RPSStore/RPSStore/Views/PayPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,31 @@ namespace RPSStore.Views
         public PayPage()
         {
             InitializeComponent();
+        }
+
+        private async void Call_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(PhoneNo.Text))
+            {
+                await Call(PhoneNo.Text);
+            }
+        }
+
+        public async Task Call(string number)
+        {
+            try
+            {
+                PhoneDialer.Open(number);
+            }
+
+            catch (FeatureNotSupportedException ex)
+            {
+                PhoneNo.Text = "Phone Dialer is not supported on this device.";
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.  
+            }
         }
     }
 }
