8980fd12ece2628432ce63a239f3f7bc41865b58
eswaribala <parameswaribala@gmail.com>
2021-05-21 00:13:36 +0530

    Inbox

---
+++ RPSStore/RPSStore/Models/Email.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPSStore.Models
+{
+    public class Email
+    {
+        public String From { get; set; }
+        public String To { get; set; }
+        public String Subject { get; set; }
+        public String Body { get; set; }
+    }
+}
---
+++ RPSStore/RPSStore/ViewModels/EmailViewModel.cs
@@ -0,0 +1,72 @@
+﻿using RPSStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace RPSStore.ViewModels
+{
+    class EmailViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        const int RefreshDuration = 2;
+       
+        readonly Random random;
+        bool isRefreshing;
+        public EmailViewModel()
+        {
+
+                random = new Random();
+                Emails = new ObservableCollection<Email>();
+                AddItems();
+            
+        }
+
+        public bool IsRefreshing
+        {
+            get { return isRefreshing; }
+            set
+            {
+                isRefreshing = value;
+                OnPropertyChanged();
+            }
+        }
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ObservableCollection<Email> Emails { get; private set; }
+
+        public ICommand RefreshCommand => new Command(async () => await 
+        RefreshItemsAsync());
+
+        private async Task RefreshItemsAsync()
+        {
+            IsRefreshing = true;
+            await Task.Delay(TimeSpan.FromSeconds(RefreshDuration));
+            AddItems();
+            IsRefreshing = false;
+        }
+
+        private void AddItems()
+        {
+            for (int i = 0; i < new Random().Next(100); i++)
+            {
+                Emails.Add(new Email
+                {
+                    From="From"+i,To="To"+i,Subject="Subject"+i,Body="Body"+i
+
+                });
+            }
+        }
+    }
+    
+}
